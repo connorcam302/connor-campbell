d9318589d0c9b7165fd3ac143e7514becd4b2b42
connorcam302 <connorcam123@gmail.com>
2023-07-24 11:24:13 +0100

    Moved from img to Image

--- src/components/StackChart.tsx
+++ src/components/StackChart.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React, { useState } from "react";
 
 type Language = {
@@ -36,9 +37,15 @@ const StackChart: React.FC = () => {
     return (
         <div className="w-full relative">
             <div className="w-full flex flex-auto font-bold text-center h-8 mt-2 mb-8">
-                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3"><div className="m-auto">Language</div></div>
-                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 w-1/3"><div className="m-auto">Framework</div></div>
-                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3"><div className="m-auto">Tools</div></div>
+                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3">
+                    <div className="m-auto">Language</div>
+                </div>
+                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 w-1/3">
+                    <div className="m-auto">Framework</div>
+                </div>
+                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3">
+                    <div className="m-auto">Tools</div>
+                </div>
             </div>
             <div className="relative z-10">
                 {experience.map((language, i) => (
@@ -49,9 +56,13 @@ const StackChart: React.FC = () => {
                         onMouseLeave={handleRowMouseLeave}
                     >
                         <div className="w-28 text-right pr-1 my-auto text-xl font-bold">{language.name}</div>
-                        <img
-                            src={`logos/${language.name}.png`}
-                            className="my-auto mx-2 w-10 h-10 transition-all z-10"
+                        <Image
+                            unoptimized={true}
+                            width={40}
+                            height={40}
+                            alt={`${language.name} Logo`}
+                            src={`/logos/${language.name}.png`}
+                            className="my-auto mx-2 transition-all z-10"
                         />
                         <div className="h-full w-1 bg-zinc-700" />
                         <div className="flex flex-auto transition-all relative">
@@ -66,7 +77,8 @@ const StackChart: React.FC = () => {
                                     className="h-3 w-full my-auto duration-200 z-10"
                                 />
                             </div>
-                            {                                    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                            {
+                                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                                 // @ts-ignore
                             }
                             {[...Array<number>(largest)].map((_, j) => (
