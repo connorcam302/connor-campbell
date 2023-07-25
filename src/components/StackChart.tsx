afd42ba5db12bd53fc6d596606bf698d14f5d865
connorcam302 <connorcam123@gmail.com>
2023-07-26 00:29:16 +0100

    Made buttons switch chart type

--- src/components/StackChart.tsx
+++ src/components/StackChart.tsx
@@ -21,30 +21,29 @@ function getYearsDifference(targetDate: Date, tillDate: Date): number {
 
 
 const languages: Experience[] = [
-    { name: "SQL", years: getYearsDifference(new Date("2020-01-25"), new Date()), colour: "#336791", hover: "#5c85a7", img: "sql.png" },
+    { name: "SQL", years: getYearsDifference(new Date("2020-01-25"), new Date()), colour: "#0079D6", hover: "#3394de", img: "sql.png" },
     { name: "Java", years: getYearsDifference(new Date("2019-09-05"), new Date()), colour: "#FF9725", hover: "#ffac51", img: "java.png" },
     { name: "JavaScript", years: getYearsDifference(new Date("2019-09-05"), new Date()), colour: "#F0DB4F", hover: "#f5e684", img: "js.png" },
     { name: "TypeScript", years: getYearsDifference(new Date("2023-02-05"), new Date()), colour: "#3178C6", hover: "#518fd4", img: "ts.png" },
     { name: "PHP", years: getYearsDifference(new Date("2019-09-05"), new Date("2021-06-06")), colour: "#4F5B93", hover: "#6471ac", img: "php.png" },
-    { name: "HTML", years: getYearsDifference(new Date("2018-09-05"), new Date()), colour: "#4F5B93", hover: "#6471ac", img: "html.png" },
-    { name: "CSS", years: getYearsDifference(new Date("2018-09-05"), new Date("2022-06-06")), colour: "#4F5B93", hover: "#6471ac", img: "css.png" },
+    { name: "HTML", years: getYearsDifference(new Date("2018-09-05"), new Date()), colour: "#E44D26", hover: "#F16529", img: "html.png" },
+    { name: "CSS", years: getYearsDifference(new Date("2018-09-05"), new Date("2022-06-06")), colour: "#264DE4", hover: "#2965F1", img: "css.png" },
 ];
 
 const framework: Experience[] = [
-    { name: "Tailwind", years: getYearsDifference(new Date("2022-09-25"), new Date()), colour: "#336791", hover: "#5c85a7", img: "sql.png" },
-    { name: "Next.js", years: getYearsDifference(new Date("2022-09-25"), new Date()), colour: "#FF9725", hover: "#ffac51", img: "java.png" },
+    { name: "Tailwind", years: getYearsDifference(new Date("2022-09-25"), new Date()), colour: "#38BDF8", hover: "#60caf9", img: "tailwind.png" },
+    { name: "Next.js", years: getYearsDifference(new Date("2022-09-25"), new Date()), colour: "#000000", hover: "#333333", img: "nextjs.png" },
     { name: "React", years: getYearsDifference(new Date("2020-09-05"), new Date()), colour: "#00D7FE", hover: "#4de3f7", img: "react.png" },
-    { name: "Node.js", years: getYearsDifference(new Date("2021-04-05"), new Date()), colour: "#00D7FE", hover: "#4de3f7", img: "react.png" },
+    { name: "Node.js", years: getYearsDifference(new Date("2021-04-05"), new Date()), colour: "#70A761", hover: "#8db981", img: "nodejs.png" },
 ];
 
 const tools: Experience[] = [
-    { name: "VS Code", years: getYearsDifference(new Date("2022-09-25"), new Date()), colour: "#336791", hover: "#5c85a7", img: "sql.png" },
-    { name: "Docker", years: getYearsDifference(new Date("2022-09-25"), new Date()), colour: "#FF9725", hover: "#ffac51", img: "java.png" },
-    { name: "Postman", years: getYearsDifference(new Date("2020-09-05"), new Date()), colour: "#00D7FE", hover: "#4de3f7", img: "react.png" },
-    { name: "NeoVim", years: getYearsDifference(new Date("2020-09-05"), new Date()), colour: "#00D7FE", hover: "#4de3f7", img: "react.png" },
-    { name: "Git", years: getYearsDifference(new Date("2021-09-05"), new Date()), colour: "#4F5B93", hover: "#6471ac", img: "php.png" },
-    { name: "AWS", years: getYearsDifference(new Date("2022-01-05"), new Date("2021-06-06")), colour: "#4F5B93", hover: "#6471ac", img: "php.png" },
-    { name: "Vercel", years: getYearsDifference(new Date("2029-25-05"), new Date("2021-06-06")), colour: "#4F5B93", hover: "#6471ac", img: "php.png" },
+    { name: "VS Code", years: getYearsDifference(new Date("2019-02-25"), new Date()), colour: "#43ACF2", hover: "#69bdf5", img: "vscode.png" },
+    { name: "Docker", years: getYearsDifference(new Date("2022-09-25"), new Date()), colour: "#066da5", hover: "#388ab7", img: "docker.png" },
+    { name: "NeoVim", years: getYearsDifference(new Date("2022-10-05"), new Date()), colour: "#5b973b", hover: "#7cac62", img: "neovim.png" },
+    { name: "Git", years: getYearsDifference(new Date("2021-02-05"), new Date()), colour: "#f05033", hover: "#f3735c", img: "git.png" },
+    { name: "AWS", years: getYearsDifference(new Date("2022-01-05"), new Date()), colour: "#223040", hover: "#384553", img: "aws.png" },
+    { name: "Vercel", years: getYearsDifference(new Date("2023-03-05"), new Date()), colour: "#000000", hover: "#333333", img: "vercel.png" },
 ];
 
 languages.sort((a, b) => b.years - a.years);
@@ -77,16 +76,16 @@ const StackChartSkeleton: React.FC<ComponentProps> = ({ experience }) => {
                 {experience.map((language, i) => (
                     <div
                         key={i}
-                        className="flex h-12"
+                        className="flex h-14"
                         onMouseEnter={() => handleRowMouseEnter(i)}
                         onMouseLeave={handleRowMouseLeave}
                     >
                         <Image
-                            width={40}
-                            height={40}
+                            width={48}
+                            height={48}
                             alt={`${language.name} Logo`}
                             src={`/static/logos/${language.img}`}
-                            className="my-auto mx-2 h-10 w-10 transition-all z-10"
+                            className="my-auto mx-2 h-12 w-12 transition-all z-10"
                         />
                         <div className="h-full w-1 bg-zinc-700" />
                         <div className="flex flex-auto transition-all relative">
@@ -144,17 +143,19 @@ const StackChart: React.FC = () => {
     return (
         <div>
             <div className="w-full flex flex-auto font-bold text-center h-12 mt-2 mb-8">
-                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3">
+                <div onClick={() => setChart(languages)} className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3">
                     <div className="m-auto">Languages</div>
                 </div>
-                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 w-1/3">
+                <div onClick={() => setChart(framework)} className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 w-1/3">
                     <div className="m-auto">Frameworks & More</div>
                 </div>
-                <div className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3">
+                <div onClick={() => setChart(tools)} className="flex flex-auto cursor-pointer hover:bg-zinc-400 duration-300 border-solid border-0 border-r border-l border-zinc-600 w-1/3">
                     <div className="m-auto">Tools</div>
                 </div>
             </div>
-            <StackChartSkeleton experience={chart} />
+            <div className="h-96">
+                <StackChartSkeleton experience={chart} />
+            </div>
         </div>)
 }
 
